49be5e510d5b2f4ff4f11a510876932b40b6dbb1
Nomso <nomsounaogu@gmail.com>
2024-07-19 21:44:34 +0100

    comm

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
     <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
     </>
   );
 }

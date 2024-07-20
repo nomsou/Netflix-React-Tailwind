2c40b5ca69282076865cf68c6eb2945fe343b32a
Nomso <nomsounaogu@gmail.com>
2024-07-20 13:55:56 +0100

    modified:   src/components/Movie.jsx modified:   src/components/Row.jsx

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -21,9 +21,9 @@ const Row = ({title, fetchURL}) => {
     <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
     <div className="relative flex items-center">
       <div id={'slider'}>
-         {movies.map((item, id) => {
+         {movies.map((item, id) => (
           <Movie item={item}/>
-         })}
+         ))}
       </div>
     </div>
     </>

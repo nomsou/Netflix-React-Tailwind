bee6cd540eafa7eae553ae5c301b720bad06a71e
Nomso <nomsounaogu@gmail.com>
2024-07-20 14:05:55 +0100

    chevron button

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { FaHeart, FaRegHeart } from 'react-icons/fa'
 import Movie from './Movie';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 const Row = ({title, fetchURL}) => {
   const [movies, setMovies] = useState([]);
@@ -18,13 +19,15 @@ const Row = ({title, fetchURL}) => {
 
   return (
     <>
-    <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-    <div className="relative flex items-center">
-      <div id={'slider'}>
+    <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+    <div className="relative flex items-center group">
+      <MdChevronLeft className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
+      <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth'>
          {movies.map((item, id) => (
           <Movie item={item}/>
          ))}
       </div>
+      <MdChevronRight className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40} />
     </div>
     </>
   );

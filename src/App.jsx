5e20d4bb9fd38c0b1dff518de46199f1ee0b3df6
Nomso <nomsounaogu@gmail.com>
2024-07-20 15:41:48 +0100

    fixed tailwind css  by adding neccesary dependencies to vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Account from "./Pages/Account";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>

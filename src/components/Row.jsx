178de289749be5e43736122abc40f116beab3e8f
Nomso <nomsounaogu@gmail.com>
2024-07-20 12:12:38 +0100

    modified:   src/components/Row.jsx

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { FaHeart, FaRegHeart } from 'react-icons/fa'
 
 const Row = (title, fetchURL) => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const[like, setLike] = useState(false);
 
   useEffect(()=> {
     axios.get(fetchURL).then((response) => {
@@ -22,6 +24,9 @@ const Row = (title, fetchURL) => {
             <img className='w-full h auto-block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
             <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opcaity-0 hover:opacity-100 text-white'>
             <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full  text-center'>{item?.title}</p>
+            <p>
+              {like ? <FaHeart className='absolute top-4 left-4 text-gray-300'/>: <FaRegHeart className='absolute top-4 left-4 text-gray-300'/>}
+            </p>
             </div>
           </div>
          })}

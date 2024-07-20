48f386e3a75b33bf4058038dea7045ae1955959a
Nomso <nomsounaogu@gmail.com>
2024-07-20 17:07:25 +0100

    firebase auth error feature

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import background_banner from "../assets/background_banner.jpg";
+import { UserAuth } from "../context/AuthContext";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState('')
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('')
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.message)
+    }
+  };
+
   return (
     <>
       <div className="w-full h-screen">
@@ -16,14 +36,17 @@ const Login = () => {
           <div className="max-w-[450px h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign In</h1>
-              <form className="w-full flex flex-col py-4">
+              {error? <p className='p-3 bg-red-400 my-2 rounded'>{error}</p>: null}
+              <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
                 <input
+                onChange={(e) => setEmail(e.target.value)}
                   className="p-3 my-2 bg-gray-700 rounded"
                   type="email"
                   placeholder="Email"
                   autoComplete="email"
                 />
                 <input
+                onChange={(e) => setPassword(e.target.value)}
                   className="p-3 my-2 bg-gray-700 rounded"
                   type="password"
                   placeholder="Password"
@@ -40,9 +63,7 @@ const Login = () => {
                   <p>Need Help?</p>
                 </div>
                 <p className="py-4">
-                  <span className="text-gray-600">
-                    New to Netflix?
-                  </span>{" "}
+                  <span className="text-gray-600">New to Netflix?</span>{" "}
                   <Link to="/signup">Sign Up</Link>
                 </p>
               </form>
@@ -51,7 +72,7 @@ const Login = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 import background_banner from "../assets/background_banner.jpg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState('')
   const { user, signUp } = UserAuth();
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError('')
     try {
       await signUp(email, password);
+      navigate('/')
     } catch (error) {
       console.log(error);
+      setError(error.message)
     }
   };
 
@@ -30,6 +35,7 @@ const Signup = () => {
           <div className="max-w-[450px h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign Up</h1>
+              {error? <p className='p-3 bg-red-400 my-2 rounded'>{error}</p>: null}
               <form
                 onSubmit={handleSubmit}
                 className="w-full flex flex-col py-4"

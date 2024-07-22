cf995bb0932338bef7876509bbf0f60382164b70
Nomso <nomsounaogu@gmail.com>
2024-07-22 16:15:51 +0100

    implementation of account component where saved shows will be displayed

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import background_banner from "../assets/background_banner.jpg";
+import SavedShows from "../components/SavedShows";
 
 const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <>
+      <div className="w-full text-white ">
+        <img
+          className="w-full h-[400px] object-cover"
+          src={background_banner}
+          alt=""
+        />
+      </div>
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]">
+        <div className="absolute-top=[20%] p-4 md:p-8">
+          <h1 className="text-5xl md:text-5xl font-bold mt-[200px]">My Shows</h1>
+        </div>
+      </div>
+      <SavedShows />
+    </>
+  );
+};
 
-export default Account
+export default Account;

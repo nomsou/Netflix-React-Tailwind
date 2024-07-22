953a4d4cddeec26f2fe78ad303e5cda809fdcd9b
Nomso <nomsounaogu@gmail.com>
2024-07-22 15:13:25 +0100

    api keys added

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore} from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBr10RNYAAmprpEAX4N8VS1zkBK2GDK8oc",
+  authDomain: "netflix-react-tailwind-d227c.firebaseapp.com",
+  projectId: "netflix-react-tailwind-d227c",
+  storageBucket: "netflix-react-tailwind-d227c.appspot.com",
+  messagingSenderId: "208670379468",
+  appId: "1:208670379468:web:c434f030f88afe81bea169"
 };
+
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+

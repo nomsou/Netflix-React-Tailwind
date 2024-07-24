b40859dc0d14d3d5d42531159c3975003729f229
Nomso <nomsounaogu@gmail.com>
2024-07-24 16:51:59 +0100

    update movie.jsx component, so when I hover it goes above the z-index

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -29,7 +29,7 @@ const Movie = ({ item }) => {
 
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-      <div className="relative overflow-hidden transform transition-transform duration-300 hover:scale-125">
+      <div className="relative overflow-hidden transform transition-transform duration-300 hover:scale-125 hover:z-20">
         <img
           className="w-full h-auto block"
           src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
